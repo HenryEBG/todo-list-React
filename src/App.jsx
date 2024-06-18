826a447b8413b526b1bdd5f7dabf0efe27ea02c5
HenryEBG <berganza386@gmail.com>
2024-06-18 10:13:43 -0400

    complete task function added

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,42 @@ import { useReducer,useRef } from 'react'
 function App() {
   //const [indexTodo, setindexTodo] = useState(0)
   const addInputRef=useRef();
+
   const [todolist,dispatch] = useReducer((state=[],action) => {
    switch (action.type) {
       case 'add_todo' :{
-        return [{title:action.title},...state];
+        
+        return [{id:Date.now(),title:action.title,isComplete:true},...state];
       }
       case 'remove_todo': {
-        return state.filter((todo,index) => index!= action.index  )
+        return state.filter((todo) => todo.id!= action.id  )
       }
+      case 'toggle_isComplete': {  
+        console.log(state)
+        return state.map(todo => {
+         if (todo.id === action.id){
+          return { ...todo,
+             isComplete:!todo.isComplete
+            }
+          }
+          return todo;
+      })
+        //return state
+      
+        // const newState=[...state];
+        // let todoIndex=newState.findIndex((todo)=> todo.id===action.id);
+        // newState[todoIndex].isComplete= !newState[todoIndex].isComplete;
+        
+        // return newState;
+}
+
+      case 'edit_todo' : {
+        //get index
+        const todoIndex=state.findIndex(todo => todo.id===action.id)
+        //replace the 
+        return state.splice(todoIndex,1,{id:action.id,title:action.title})
+      }
+
       default : {
         return state;
       }    
@@ -43,12 +71,14 @@ function App() {
   {/* list of todos  */}
     <div>
       {
-       todolist && todolist.map((todo,index) => (
-         <div className='todoItem' key={index}>
-              <input type="checkbox" />
+       todolist && todolist.map((todo) => (
+         <div className='todoItem' key={todo.id}>
+           
+              <input type="checkbox" checked={todo.isComplete} onChange={() => dispatch({type:'toggle_isComplete',id:todo.id})}/>
               <p>{todo.title}</p>
-              <button>Edit</button>
-              <button onClick={() => dispatch({type: 'remove_todo', index })}>Delete</button>
+              <button  onClick={() => dispatch({type: 'edit_todo', id:todo.id, title:todo.title+"modificado"})}>Edit</button>
+              <button onClick={() => dispatch({type: 'remove_todo', id:todo.id })}>Delete</button>
+             
           </div>
         ))
       }
